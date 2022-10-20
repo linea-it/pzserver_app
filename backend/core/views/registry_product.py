c8cbbc477a7ccbf6392262475909060e017e945b
glaubervila <glauber.vila.verde@gmail.com>
2022-10-20 16:13:45 -0300

    Fixed bug when importing some csv files. errors related to not being able to identify the headers and the delimiter.

--- backend/core/views/registry_product.py
+++ backend/core/views/registry_product.py
@@ -1,12 +1,9 @@
-import csv
 import logging
-import os
 import shutil
-import zipfile
 from pathlib import Path
 
-import pandas as pd
 from core.models import Product, ProductContent
+from core.product_handle import ProductHandle
 
 
 class RegistryProduct:
@@ -31,20 +28,6 @@ class RegistryProduct:
             self.log = logging.getLogger("registry_product")
         return self.log
 
-    # def get_path(self):
-
-    #     product_type = self.product.product_type.name
-
-    #     path = Path(
-    #         settings.MEDIA_ROOT,
-    #         product_type,
-    #         self.product.internal_name,
-    #     )
-    #     path.mkdir(parents=True, exist_ok=True)
-
-    #     self.log.info("Product Path: [%s]" % path)
-    #     return path
-
     def registry(self):
         try:
             # Alterar o Internal name
@@ -63,16 +46,12 @@ class RegistryProduct:
             self.main_file = Path(mf.file.path)
             self.log.info("Main File: [%s]" % self.main_file)
 
-            # Identificar o formato do arquivo principal
-            extension = self.main_file.suffix
-
-            match extension:
-                case ".csv":
-                    self.handle_csv()
-                case _:
-                    message = f"The {extension} extension has not yet been implemented"
-                    self.log.warning(message)
-                    raise Exception(message)
+            # Le o arquivo principal e converte para pandas.Dataframe
+            df_product = ProductHandle().df_from_file(self.main_file, nrows=5)
+            # Lista de Colunas no arquivo.
+            product_columns = df_product.columns.tolist()
+            # Registra as colunas do produto no banco de dados.
+            self.create_product_contents(product_columns)
 
             # Salva as alterações feitas no model product
             self.product.save()
@@ -80,67 +59,6 @@ class RegistryProduct:
         except Exception as e:
             self.log.error(e)
             raise Exception(e)
-
-    def handle_csv(self):
-        self.log.info("Using csv format to process main file.")
-        csv_file = self.main_file
-
-        # Identificar se o csv tem Headers
-        has_header = self.csv_has_header(csv_file)
-
-        columns = list()
-        if has_header:
-            # Identificar as colunas
-            columns = self.csv_column_names(csv_file)
-        else:
-            # TODO: Implementar função para criar colunas fakes
-            pass
-
-        # Registra as colunas do produto no banco de dados:
-        self.create_product_contents(columns)
-
-    def csv_has_header(self, path):
-        with open(path, "r") as f:
-            try:
-                has_header = csv.Sniffer().has_header(f.read(2048))
-                self.log.info("Has Header: [%s]" % has_header)
-
-                return has_header
-            except csv.Error as e:
-                message = f"Failed to identify csv headers. {e}"
-                self.log.error(message)
-                raise Exception(message)
-
-    def csv_identify_delimiter(self, path):
-        """Identifica o delimitador e o dialect do csv."""
-
-        with open(path, "r") as f:
-            try:
-                dialect = csv.Sniffer().sniff(f.read(2048), delimiters=",\t :;")
-                delimiter = dialect.delimiter
-                self.log.info("Delimiter: [%s]" % delimiter)
-                return delimiter
-            except csv.Error as e:
-                self.log.error(e)
-                raise Exception(e)
-
-    def csv_column_names(self, path):
-
-        try:
-            # Identificar o Delimitador do csv
-            delimiter = self.csv_identify_delimiter(path)
-
-            # Cria um pandas dataframe para recuperar as colunas
-            df = pd.read_csv(path, sep=delimiter)
-            columns = list(map(str.strip, df.columns))
-            self.log.info(f"Columns: {len(columns)}")
-
-            return columns
-
-        except Exception as e:
-            message = f"Could not identify csv columns. {e}"
-            self.log.error(message)
-            raise Exception(message)
 
     def create_product_contents(self, columns):
         """Registrar as colunas na tabela Product Contents
@@ -178,34 +96,6 @@ class RegistryProduct:
             self.log.error(message)
             raise Exception(message)
 
-    # def create_zip(self, from_path, to_path, compresslevel=0):
-    #     """
-    #     Comprime um diretorio e seus arquivos para um unico arquivo .zip.
-    #     """
-    #     try:
-    #         self.log.info(f"Compressing the directory: {from_path}")
-
-    #         with zipfile.ZipFile(
-    #             to_path,
-    #             "w",
-    #             compression=zipfile.ZIP_DEFLATED,
-    #             compresslevel=compresslevel,
-    #         ) as ziphandle:
-    #             for root, dirs, files in os.walk(from_path):
-    #                 for file in files:
-    #                     origin_file = os.path.join(root, file)
-    #                     self.log.debug("Adding File: %s" % origin_file)
-    #                     ziphandle.write(origin_file, arcname=file)
-
-    #         ziphandle.close()
-
-    #         self.log.info(f"Compressed directory: {to_path}")
-
-    #     except Exception as e:
-    #         message = f"Failed to store product files. {e}"
-    #         self.log.error(message)
-    #         raise Exception(message)
-
     def remove_dir(self, path):
         try:
             shutil.rmtree(path)
@@ -214,34 +104,3 @@ class RegistryProduct:
             message = f"Failed to register the product. {e}"
             self.log.error(message)
             raise Exception(message)
-
-
-# from django.core.files import File
-# from django.core.files.base import ContentFile
-# from django.conf import settings
-# # Criar o path definitivo para o produto
-# new_path = self.get_path()
-# # TODO: Guardar o path para o diretório no model.
-
-# # Diretório temporario onde foi feito o upload dos arquivos
-# temp_dir_path = self.main_file.parent
-
-# # Path para o arquivo zip com todo o conteudo do produto
-# zip_name = f"{self.product.internal_name.split('_')[1]}.zip"
-# zip_path = Path(new_path, zip_name)
-
-# # Comprimir o diretório e todos os arquivos do produto.
-# self.create_zip(temp_dir_path, zip_path, 9)
-
-# # Atualiza o Produto, o main_file agora passa a ser o zip do produto.
-# relative_main_file = str(zip_path).split(settings.MEDIA_ROOT)[1]
-# self.log.debug(relative_main_file)
-# self.product.main_file = relative_main_file
-# self.product.save()
-
-# self.product.file_name = zip_path.name
-# self.product.file_size = zip_path.stat().st_size
-# self.product.file_extension = zip_path.suffix
-
-# # Apaga o diretório temporario
-# self.remove_dir(temp_dir_path)
