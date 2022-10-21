e49ebb3faa0c5168afd7daed785edc2e9d6952f0
Cristiano Singulani <crisingulani@gmail.com>
2022-10-21 15:21:10 -0300

    Added handling in case of non-default delimiter in csv files (comma)

--- backend/core/product_handle.py
+++ backend/core/product_handle.py
@@ -4,6 +4,7 @@ from typing import List
 
 import pandas as pd
 import tables_io
+import csv
 
 from core._typing import Column, PathLike
 
@@ -39,17 +40,7 @@ class FileHandle(object):
         match extension:
             case ".csv":
                 self._handle = CsvHandle(fp)
-            case ".fits":
-                self._handle = TableIOHandle(fp)
-            case ".fit":
-                self._handle = TableIOHandle(fp)
-            case ".hf5":
-                self._handle = TableIOHandle(fp)
-            case ".hdf5":
-                self._handle = TableIOHandle(fp)
-            case ".h5":
-                self._handle = TableIOHandle(fp)
-            case ".pq":
+            case ".fits" | ".fit" | ".hf5" | ".hdf5" | ".h5" | ".parquet" | ".pq":
                 self._handle = TableIOHandle(fp)
             # TODO: .zip, .tar, .tar.gz
             case _:
@@ -73,6 +64,7 @@ class BaseHandle(object):
 
 class CsvHandle(BaseHandle):
 
+    delimiter = str
     has_hd = bool  # True se o arquivo CSV possuir Headers na primeira linha.
     column_names = [Column]  # Lista com nome das colunas que podem ser str ou int.
 
@@ -80,6 +72,7 @@ class CsvHandle(BaseHandle):
 
         super().__init__(filepath)
 
+        self.delimiter = self.get_delimiter()
         self.has_hd = self.has_header()
         self.column_names = self.get_column_names()
 
@@ -90,6 +83,13 @@ class CsvHandle(BaseHandle):
             raise Exception(
                 "It is not possible to use the header argument in the df_from_file() method when the file is a .csv file."
             )
+
+        if "delimiter" in kwargs:
+            raise Exception(
+                "It is not possible to use the delimiter argument in the df_from_file() method when the file is a .csv file."
+            )
+
+        kwargs.update({"delimiter": self.delimiter})
 
         if self.has_hd:
             df = pd.read_csv(self.filepath, header=0, **kwargs)
@@ -117,8 +117,8 @@ class CsvHandle(BaseHandle):
         # Method: open csv twice considering with header and without header,
         # if the data types are the same in both times it probably doesn't have header.
         # https://stackoverflow.com/questions/53100598/can-pandas-auto-recognize-if-header-is-present/53101192#53101192
-        df = pd.read_csv(self.filepath, header=None, nrows=20)
-        df_header = pd.read_csv(self.filepath, nrows=20)
+        df = pd.read_csv(self.filepath, header=None, delimiter=self.delimiter, nrows=20)
+        df_header = pd.read_csv(self.filepath, delimiter=self.delimiter, nrows=20)
         if tuple(df.dtypes) != tuple(df_header.dtypes):
             temp.append(True)
         else:
@@ -126,7 +126,6 @@ class CsvHandle(BaseHandle):
 
         # Method: Check if at least one in first Line is String.
         # https://stackoverflow.com/questions/38360496/pandas-read-csv-without-knowing-whether-header-is-present/62217716#62217716
-        df = pd.read_csv(self.filepath, header=None, nrows=20)
         if any(df.iloc[0].apply(lambda x: isinstance(x, str))):
             temp.append(True)
         else:
@@ -135,17 +134,29 @@ class CsvHandle(BaseHandle):
 
     def get_column_names(self) -> List[Column]:
 
+        df = pd.read_csv(self.filepath, header=None, delimiter=self.delimiter, nrows=5)
+
         if self.has_hd:
-            df = pd.read_csv(self.filepath, header=None, nrows=5)
             df = df[1:].reset_index(drop=True).rename(columns=df.iloc[0])
             columns = df.columns.tolist()
         else:
             # Cria nomes para as colunas de forma sequencial [0...len(headers)]
             # o Resultado é uma lista de str: ['0', ...,'10',...]
-            df = pd.read_csv(self.filepath, header=None, nrows=5)
             columns = [str(i) for i in [*range(0, len(df.iloc[0]))]]
 
         return columns
+
+    def get_delimiter(self):
+        """ Get delimiter from file
+
+        Returns:
+            str: delimiter
+        """
+        with open(self.filepath, 'r') as csvfile:
+            dt = csvfile.readline().replace('\n','')
+            delimiter = csv.Sniffer().sniff(dt).delimiter
+
+        return delimiter
 
 
 class TableIOHandle(BaseHandle):
@@ -157,9 +168,6 @@ class TableIOHandle(BaseHandle):
         # TODO: Tratar arquivos com mais de uma tabela.
 
         # Le o arquivo utilizando o metodo read da tables_io
-        # O retorno é um astropy table.
-        tb_ap = tables_io.read(self.filepath)
-        # Converte o astropy table para pandas.Dataframe
-        df = tables_io.convert(tb_ap, tables_io.types.PD_DATAFRAME)
+        df = tables_io.read(self.filepath, tables_io.types.PD_DATAFRAME)
 
         return df
