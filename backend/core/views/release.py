b03d9cc7282e58ab8ed52f46d38e908a60e3d2f8
Cristiano Singulani <crisingulani@gmail.com>
2022-10-28 18:54:47 -0300

    Corrigido problema com filtros de product_type e release. (#65)
    As viewers Product Type e Release foram protegidas,
    permitindo delete e update somente pelo admin.

--- backend/core/views/release.py
+++ backend/core/views/release.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 from core.serializers import ReleaseSerializer
 from core import models
 
 
 class ReleaseViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUser, IsAuthenticatedOrReadOnly]
     queryset = models.Release.objects.all()
     serializer_class = ReleaseSerializer
     ordering = ["-created_at"]
