63f3e178954049bfb986105c2e77e27407f1a1ee
Glauber Costa Vila-Verde <glauber.vila.verde@gmail.com>
2022-11-07 18:48:41 -0300

    Added text to Tutorial Pages (#76)

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -1,105 +1,115 @@
 import { Container, Grid, Typography, Link } from '@mui/material'
 import useStyles from '../styles/pages/about'
 import { parseCookies } from 'nookies'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
 export default function About() {
   const classes = useStyles()
   return (
     <Container className={classes.root}>
       <Grid container spacing={8}>
         <Grid item xs={12}>
-          <Typography variant="h6">About</Typography>
-          <Typography variant="body1" component="span">
-            <p>
-              Inspired by features of the DES Science Portal (
-              <Link
-                href="https://www.sciencedirect.com/science/article/abs/pii/S2213133718300891?via%3Dihub"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Gschwend et al., 2018;
-              </Link>{' '}
-              <Link
-                href="https://www.sciencedirect.com/science/article/abs/pii/S2213133717300975"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Fausti Neto et al., 2018
-              </Link>{' '}
-              ), the Photo-z Server is an online service, complementary to the
-              Rubin Science Platform (RSP), to host photo-z-related lightweight
-              data products and to offer data management tools that allow
-              sharing data products among RSP users, attach and share relevant
-              metadata, and help on provenance tracking. It is delivered as part
-              of the in-kind contribution program BRA-LIN. An overview of this
-              and other contributions is available{' '}
-              <Link
-                href="https://linea-it.github.io/pz-lsst-inkind-doc/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                here.
-              </Link>{' '}
-              As required by the LSST in-kind program, the source code is
-              publicly available on{' '}
-              <Link
-                href="https://github.com/linea-it/pz-server"
-                target="_blank"
-                rel="noreferrer"
-              >
-                GitHub.
-              </Link>
-            </p>
-            <p>
-              The Photo-z Server is hosted in the Brazilian Independent Data
-              Access Center (IDAC) and is open to all RSP users (LSST data
-              rights holders), without geographic constraints. It is designed to
-              be as broad and generic as possible to be useful to all LSST
-              Science Collaborations that work with photo-z data products.
-            </p>
-            <p>
-              The Photo-z Server is being designed with a special focus on
-              helping RSP users participating in the Photo-z Validation
-              Cooperative, a DM team&apos;s initiative that will take place
-              during LSST commissioning phase (see technical note dmtn-049 for
-              details), but it is planned to continue serving the LSST Community
-              during subsequent years. During the Photo-z Validation
-              Cooperative, the Photo-z Coordination Group will be able to use
-              the Photo-z Server to host and distribute standardized training
-              and validation sets to be used in algorithm performance comparison
-              experiments, as well as to collect the results obtained by
-              different users.
-            </p>
-            <p>
-              Beyond the Photo-z Validation Cooperative, the RSP users will be
-              able to use the Photo-z Server to easily keep track and share
-              lightweight files containing varied test results. All data
-              products uploaded to the Photo-z Server will automatically be
-              visible and available, without any scientific validation, to all
-              RSP users, and only for this particular group, therefore it is not
-              the appropriate tool to release data products to the general
-              public.
-            </p>
-            <p>
-              If you have comments or suggestions, be welcome to open an issue
-              on the{' '}
-              <Link
-                href="https://github.com/linea-it/pz-server/issues"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Photo-z Server repository on GitHub
-              </Link>
-              , or contact the developers at{' '}
-              <Link
-                href="mailto:helpdesk@linea.org.br"
-                target="_blank"
-                rel="noreferrer"
-              >
-                helpdesk@linea.org.br
-              </Link>
-              .
-            </p>
+          <Typography variant="h6" sx={{ mb: 2 }}>
+            About
           </Typography>
+          <Card>
+            <CardContent>
+              <Typography variant="body1" component="span">
+                <p>
+                  Inspired by features of the DES Science Portal (
+                  <Link
+                    href="https://www.sciencedirect.com/science/article/abs/pii/S2213133718300891?via%3Dihub"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Gschwend et al., 2018;
+                  </Link>{' '}
+                  <Link
+                    href="https://www.sciencedirect.com/science/article/abs/pii/S2213133717300975"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Fausti Neto et al., 2018
+                  </Link>{' '}
+                  ), the Photo-z Server is an online service, complementary to
+                  the Rubin Science Platform (RSP), to host photo-z-related
+                  lightweight data products and to offer data management tools
+                  that allow sharing data products among RSP users, attach and
+                  share relevant metadata, and help on provenance tracking. It
+                  is delivered as part of the in-kind contribution program
+                  BRA-LIN. An overview of this and other contributions is
+                  available{' '}
+                  <Link
+                    href="https://linea-it.github.io/pz-lsst-inkind-doc/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    here.
+                  </Link>{' '}
+                  As required by the LSST in-kind program, the source code is
+                  publicly available on{' '}
+                  <Link
+                    href="https://github.com/linea-it/pz-server"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    GitHub.
+                  </Link>
+                </p>
+                <p>
+                  The Photo-z Server is hosted in the Brazilian Independent Data
+                  Access Center (IDAC) and is open to all RSP users (LSST data
+                  rights holders), without geographic constraints. It is
+                  designed to be as broad and generic as possible to be useful
+                  to all LSST Science Collaborations that work with photo-z data
+                  products.
+                </p>
+                <p>
+                  The Photo-z Server is being designed with a special focus on
+                  helping RSP users participating in the Photo-z Validation
+                  Cooperative, a DM team&apos;s initiative that will take place
+                  during LSST commissioning phase (see technical note dmtn-049
+                  for details), but it is planned to continue serving the LSST
+                  Community during subsequent years. During the Photo-z
+                  Validation Cooperative, the Photo-z Coordination Group will be
+                  able to use the Photo-z Server to host and distribute
+                  standardized training and validation sets to be used in
+                  algorithm performance comparison experiments, as well as to
+                  collect the results obtained by different users.
+                </p>
+                <p>
+                  Beyond the Photo-z Validation Cooperative, the RSP users will
+                  be able to use the Photo-z Server to easily keep track and
+                  share lightweight files containing varied test results. All
+                  data products uploaded to the Photo-z Server will
+                  automatically be visible and available, without any scientific
+                  validation, to all RSP users, and only for this particular
+                  group, therefore it is not the appropriate tool to release
+                  data products to the general public.
+                </p>
+                <p>
+                  If you have comments or suggestions, be welcome to open an
+                  issue on the{' '}
+                  <Link
+                    href="https://github.com/linea-it/pz-server/issues"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Photo-z Server repository on GitHub
+                  </Link>
+                  , or contact the developers at{' '}
+                  <Link
+                    href="mailto:helpdesk@linea.org.br"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    helpdesk@linea.org.br
+                  </Link>
+                  .
+                </p>
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
       </Grid>
     </Container>
--- frontend/pages/contact.js
+++ frontend/pages/contact.js
@@ -1,47 +1,55 @@
 import { Container, Grid, Typography, Link } from '@mui/material'
 import useStyles from '../styles/pages/contact'
 import { parseCookies } from 'nookies'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
 export default function Contact() {
   const classes = useStyles()
   return (
     <Container className={classes.root}>
       <Grid container spacing={2}>
         <Grid item xs={12}>
-          <Typography variant="h6">Contact</Typography>
-          <Typography variant="body1" component="span">
-            <p>Comments, questions, suggestions?</p>
-            <p>
-              Be welcome to open an issue on the{' '}
-              <Link
-                href="https://github.com/linea-it/pz-server"
-                target="_blank"
-                rel="noreferrer"
-              >
-                PZ Server repository on GitHub
-              </Link>
-              , or contact our team.
-            </p>
-            <p>
-              BRA-LIN S2 Contribution Lead:{' '}
-              <Link
-                href="mailto:julia@linea.org.br"
-                target="_blank"
-                rel="noreferrer"
-              >
-                julia@linea.org.br
-              </Link>
-            </p>
-            <p>
-              Technical support:{' '}
-              <Link
-                href="mailto:helpdesk@linea.org.br"
-                target="_blank"
-                rel="noreferrer"
-              >
-                helpdesk@linea.org.br
-              </Link>
-            </p>
+          <Typography variant="h6" sx={{ mb: 2 }}>
+            Contact
           </Typography>
+          <Card>
+            <CardContent>
+              <Typography variant="body1" component="span">
+                <p>Comments, questions, suggestions?</p>
+                <p>
+                  Be welcome to open an issue on the{' '}
+                  <Link
+                    href="https://github.com/linea-it/pz-server"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    PZ Server repository on GitHub
+                  </Link>
+                  , or contact our team.
+                </p>
+                <p>
+                  BRA-LIN S2 Contribution Lead:{' '}
+                  <Link
+                    href="mailto:julia@linea.org.br"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    julia@linea.org.br
+                  </Link>
+                </p>
+                <p>
+                  Technical support:{' '}
+                  <Link
+                    href="mailto:helpdesk@linea.org.br"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    helpdesk@linea.org.br
+                  </Link>
+                </p>
+              </Typography>
+            </CardContent>{' '}
+          </Card>
         </Grid>
       </Grid>
     </Container>
