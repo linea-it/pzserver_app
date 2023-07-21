b2dc556153129184d69bb7502d9290fc5962587f
Jandson Vitorino <jandson1512@gmail.com>
2023-07-21 16:04:08 -0300

    lint adjusts

--- frontend/components/ProductShare.js
+++ frontend/components/ProductShare.js
@@ -1,9 +1,9 @@
 import {
-    Button,
-    Dialog,
-    DialogContent,
-    DialogTitle,
-    TextField
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  TextField
 } from '@mui/material'
 import DialogActions from '@mui/material/DialogActions'
 import InputAdornment from '@mui/material/InputAdornment'
@@ -12,60 +12,60 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 export default function ProductShare({
-    isOpen,
-    handleShareDialogOpen,
-    url,
-    setParentSnackbarOpen
+  isOpen,
+  handleShareDialogOpen,
+  url,
+  setParentSnackbarOpen
 }) {
-    const handleCopyUrl = () => {
-        navigator.clipboard
-            .writeText(url)
-            .then(() => {
-                handleShareDialogOpen()
-                setParentSnackbarOpen(true)
-            })
-            .catch(error => {
-                console.error(error)
-            })
-    }
+  const handleCopyUrl = () => {
+    navigator.clipboard
+      .writeText(url)
+      .then(() => {
+        handleShareDialogOpen()
+        setParentSnackbarOpen(true)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
 
-    return (
-        <Dialog
-            open={isOpen}
-            onClose={handleShareDialogOpen}
-            PaperProps={{
-                style: { width: '500px', minHeight: '150px' }
-            }}
-        >
-            <DialogTitle style={{ fontSize: '16px' }}>
-                Copy the download URL:
-            </DialogTitle>
-            <DialogContent>
-                <TextField
-                    fullWidth
-                    variant="outlined"
-                    value={url}
-                    InputProps={{
-                        endAdornment: (
-                            <InputAdornment position="end">
-                                <Button variant="contained" onClick={handleCopyUrl}>
-                                    Copy
-                                </Button>
-                            </InputAdornment>
-                        )
-                    }}
-                />
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={handleShareDialogOpen}>Close</Button>
-            </DialogActions>
-        </Dialog>
-    )
+  return (
+    <Dialog
+      open={isOpen}
+      onClose={handleShareDialogOpen}
+      PaperProps={{
+        style: { width: '500px', minHeight: '150px' }
+      }}
+    >
+      <DialogTitle style={{ fontSize: '16px' }}>
+        Copy the download URL:
+      </DialogTitle>
+      <DialogContent>
+        <TextField
+          fullWidth
+          variant="outlined"
+          value={url}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <Button variant="contained" onClick={handleCopyUrl}>
+                  Copy
+                </Button>
+              </InputAdornment>
+            )
+          }}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleShareDialogOpen}>Close</Button>
+      </DialogActions>
+    </Dialog>
+  )
 }
 
 ProductShare.propTypes = {
-    isOpen: PropTypes.bool.isRequired,
-    handleShareDialogOpen: PropTypes.func.isRequired,
-    url: PropTypes.string,
-    setParentSnackbarOpen: PropTypes.func.isRequired
+  isOpen: PropTypes.bool.isRequired,
+  handleShareDialogOpen: PropTypes.func.isRequired,
+  url: PropTypes.string,
+  setParentSnackbarOpen: PropTypes.func.isRequired
 }
