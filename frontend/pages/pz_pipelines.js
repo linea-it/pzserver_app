d0bac436f5fad7ff87d87501ed7c57bab43ae86a
Jandson Vitorino <jandson1512@gmail.com>
2024-03-12 13:45:15 -0300

    layout corrections and addition of back to previous page buttons

--- frontend/pages/pz_pipelines.js
+++ frontend/pages/pz_pipelines.js
@@ -1,18 +1,37 @@
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import Link from '@mui/material/Link'
+import Breadcrumbs from '@mui/material/Breadcrumbs'
+import ArrowBackIos from '@mui/icons-material/ArrowBackIos'
+import IconButton from '@mui/material/IconButton'
 import React from 'react'
-import { Grid, Typography } from '@mui/material'
 import SpeczCatalogs from '../components/SpeczCatalogs'
 import TrainingSetMaker from '../components/TrainingSetMaker'
 
 function PZPipelines() {
   return (
-    <Grid container direction="column" alignItems="center">
+    <Grid container direction="column" alignItems="center" mb={10}>
       <Typography
         variant="h4"
         align="center"
         color="textPrimary"
-        mb={10}
-        mt={-12}
+        mb={5}
+        mr={55}
       >
+        <Breadcrumbs aria-label="breadcrumb">
+          <Link color="inherit" href="/">
+            Home
+          </Link>
+          <Typography color="textPrimary">Pipelines</Typography>
+        </Breadcrumbs>
+        <IconButton
+            color="primary"
+            edge="start"
+            aria-label="Go back"
+            onClick={() => window.history.back()}
+          >
+            <ArrowBackIos />
+          </IconButton>
         Photo-z Server Pipelines
       </Typography>
 
