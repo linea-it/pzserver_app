6ad1b4669aeac2717a47d692025637de39a386f1
Cristiano Singulani <crisingulani@gmail.com>
2024-06-13 17:41:50 +0000

    Removed shibboleth

--- backend/pzserver/test_settings.py
+++ backend/pzserver/test_settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # "django.contrib.sites",
     # Third-party
     "corsheaders",
     "django_filters",
@@ -48,7 +47,6 @@ INSTALLED_APPS = [
     "oauth2_provider",
     "social_django",
     "drf_social_oauth2",
-    "shibboleth",
     # Apps
     "core",
 ]
@@ -59,7 +57,6 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "core.shibboleth.ShibbolethMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -182,28 +179,10 @@ JSON_EDITOR = True
 AUTHENTICATION_BACKENDS = (
     "drf_social_oauth2.backends.DjangoOAuth2",
     "django.contrib.auth.backends.ModelBackend",
-    "shibboleth.backends.ShibbolethRemoteUserBackend",
 )
 
 ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS = True
 LOGIN_REDIRECT_URL = "/"
-
-# Shibboleth Authentication
-SHIBBOLETH_ENABLED = False
-if os.getenv("AUTH_SHIB_URL", None) is not None:
-    # https://github.com/Brown-University-Library/django-shibboleth-remoteuser
-    SHIBBOLETH_ATTRIBUTE_MAP = {
-        "eppn": (True, "username"),
-        "cn": (True, "first_name"),
-        "sn": (True, "last_name"),
-        "Shib-inetOrgPerson-mail": (True, "email"),
-    }
-    SHIBBOLETH_GROUP_ATTRIBUTES = "Shibboleth"
-    # Including Shibboleth authentication:
-    AUTHENTICATION_BACKENDS += (
-        "shibboleth.backends.ShibbolethRemoteUserBackend",)
-
-    SHIBBOLETH_ENABLED = True
 
 OAUTH2_PROVIDER = {
     "ACCESS_TOKEN_EXPIRE_SECONDS": 36000,
