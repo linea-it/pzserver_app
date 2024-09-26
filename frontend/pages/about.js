0bdbe912ce586567c78aae406f75788fdfb7d3c2
jandsonrj <jandson1512@gmail.com>
2024-09-26 20:35:04 +0000

    fixes ESLint errors

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -131,7 +131,7 @@ export default function About() {
               </Typography>
             </CardContent>
           </Card>
-      <Partnersfooter />
+          <Partnersfooter />
         </Grid>
       </Grid>
     </Container>
