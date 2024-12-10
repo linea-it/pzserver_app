f6a1b829a5aee242954a33ec12640ad2695acaaa
jandsonrj <jandson1512@gmail.com>
2024-12-10 14:05:19 -0300

    Fix: images on broken on login page

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: ['identity.linea.org.br'],
-  },
   async rewrites() {
     return [{ source: '/front-api/:path*', destination: '/api/:path*' }]
   },
