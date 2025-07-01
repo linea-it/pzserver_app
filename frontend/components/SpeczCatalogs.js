98d0f11a4d75c091be054f866dfc1d1d6690ba0a
Cristiano Singulani <crisingulani@gmail.com>
2025-07-01 17:41:21 -0300

    feat: enable links and update text in TrainingSetMaker and SpeczCatalogs (#465)
    * feat: enable links and update text in TrainingSetMaker and SpeczCatalogs
    
    This commit introduces the following changes:
    
    - Enables links to the Training Set Maker and Spec-z Catalogs pages.
    - Updates the text in the TrainingSetMaker and SpeczCatalogs components to remove the "(soon)" suffix and improve clarity.
    - Renames "Spec-z Catalogs" alt text to "Redshift Catalogs" in SpeczCatalogs component.
    
    * feat: improve TrainingSetMaker and SpeczCatalogs components
    
    This commit improves the TrainingSetMaker and SpeczCatalogs components by:
    
    - Removing unnecessary line breaks in the Typography variant h5 elements for both components, improving readability.

--- frontend/components/SpeczCatalogs.js
+++ frontend/components/SpeczCatalogs.js
@@ -2,37 +2,36 @@ import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
+import Link from '@mui/material/Link'
 import Typography from '@mui/material/Typography'
 
 function SpeczCatalogs() {
   return (
-    // <Link href="/specz_catalogs">
-    <Card
-      elevation={3}
-      sx={{
-        display: 'flex'
-      }}
-    >
-      <CardMedia
-        component="img"
-        sx={{ width: 350, height: 231 }}
-        image="../interfaces/milkyway.jpg"
-        alt="Spec-z Catalogs"
-      />
-      <Box>
-        <CardContent m={2} sx={{ maxWidth: 500 }}>
-          <Typography color="#888" variant="h5">
-            Combine Redshift Catalogs (soon)
-          </Typography>
-          <Typography variant="body1" color="#555" sx={{ mt: 1 }}>
-            Creates a single redshift sample from the multiple spatial
-            cross-matching (all-to-all) of a list of pre-registered individual
-            Redshift Catalogs.
-          </Typography>
-        </CardContent>
-      </Box>
-    </Card>
-    // </Link>
+    <Link href="/specz_catalogs">
+      <Card
+        elevation={3}
+        sx={{
+          display: 'flex'
+        }}
+      >
+        <CardMedia
+          component="img"
+          sx={{ width: 350, height: 231 }}
+          image="../interfaces/milkyway.jpg"
+          alt="Redshift Catalogs"
+        />
+        <Box>
+          <CardContent m={2} sx={{ maxWidth: 500 }}>
+            <Typography variant="h5">Combine Redshift Catalogs</Typography>
+            <Typography variant="body1" sx={{ mt: 1 }}>
+              Creates a single redshift sample from the multiple spatial
+              cross-matching (all-to-all) of a list of pre-registered individual
+              Redshift Catalogs.
+            </Typography>
+          </CardContent>
+        </Box>
+      </Card>
+    </Link>
   )
 }
 
