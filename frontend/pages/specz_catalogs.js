934a15839bcf35bce8b34a45068d3e1060f2553e
Cristiano Singulani <crisingulani@gmail.com>
2025-08-04 12:00:07 -0300

    fix: improve specz catalogs UI (#485)
    * fix: improve specz catalogs UI
    
    - Moves the Card component containing the SpeczData component outside of the Grid item with xs=12. This likely corrects a layout issue or improves responsiveness.
    - Updates the orchestration/pipelines submodule.
    
    * chore: update subproject commit reference in pipelines

--- frontend/pages/specz_catalogs.js
+++ frontend/pages/specz_catalogs.js
@@ -255,8 +255,6 @@ function SpeczCatalogs() {
               </Typography>
               {/* <SearchField onChange={query => setSearch(query)} /> */}
             </Box>
-          </Grid>
-          <Grid item xs={12}>
             <Card>
               <CardContent>
                 <SpeczData
